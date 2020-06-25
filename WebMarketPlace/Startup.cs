0220da17a299e300e05d55a40b38d628ddda6684
Shushlayka2 <inashur68@gmail.com>
2020-06-25 11:30:47 +0300

    .vs directory excluded

--- WebMarketPlace/Startup.cs
+++ WebMarketPlace/Startup.cs
@@ -1,44 +1,36 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using WebMarketPlace.Models;
+﻿using WebMarketPlace.Models;
 
 namespace WebMarketPlace
 {
     public class Startup
     {
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
-
-		public IConfiguration Configuration { get; }
-
-		public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
-			string connection = Configuration.GetConnectionString("DefaultConnection");
-			services.AddDbContext<MarketDBContext>(options => options.UseNpgsql(connection));
-			services.AddMvc();
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options => 
-				{
-					options.RequireHttpsMetadata = false;
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuer = true,
-						ValidIssuer = AuthOptions.ISSUER,
-						ValidateAudience = true,
-						ValidAudience = AuthOptions.AUDIENCE,
-						ValidateLifetime = true,
-						IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-						ValidateIssuerSigningKey = true,
-					};
-				});
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MarketDBContext>(options => options.UseNpgsql(connection));
+            services.AddMvc();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.ISSUER,
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -46,19 +38,19 @@ namespace WebMarketPlace
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-				app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-				{
-					HotModuleReplacement = true
-				});
-			}
-			app.UseStaticFiles();
-			app.UseAuthentication();
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
-		}
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
